336b5ef94cf89e5175d9de53ce26dac4b5ff16c4
ZTaylor97 <zacktaylor97@gmail.com>
2024-05-08 21:23:55 +1000

    GB-7: GB-12 complete, more work needed on making clean generic instructions

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -1,16 +1,17 @@
 mod cpu_registers;
 
+use num_traits::NumAssignRef;
 use std::default;
 
 use self::cpu_registers::CPURegisters;
 use super::instructions::Instruction;
 
-pub struct CPU {
-    instructions: [Instruction; 255],
+pub struct CPU<T: NumAssignRef> {
+    instructions: [Instruction<T>; 255],
     registers: CPURegisters,
 }
 
-impl Default for CPU {
+impl<T: NumAssignRef> Default for CPU<T> {
     fn default() -> Self {
         Self {
             instructions: todo!(),
--- src/emulator/instructions.rs
+++ src/emulator/instructions.rs
@@ -1,34 +1,46 @@
-pub struct Instruction {
+use num_traits::NumAssignRef;
+
+pub struct Instruction<T: NumAssignRef> {
     mnemonic: String,
     bytes: u8,
     cycles: u8,
+    func: fn(Operands<T>) -> (),
 }
-enum Operands<'a> {
+pub enum Operands<'a, T: NumAssignRef> {
     None,
-    One(&'a u8),
-    OneMut(&'a mut u8),
-    Two(&'a u8, &'a u8),
-    TwoOneMut(&'a mut u8, &'a u8),
-    TwoMut(&'a mut u8, &'a mut u8),
+    One(T),
+    OneMut(&'a mut T),
+    Two(T, T),
+    TwoOneMut(&'a mut T, T),
+    TwoMut(&'a mut T, &'a mut T),
 }
-
-impl Default for Instruction {
+impl<T> Default for Instruction<T>
+where
+    T: NumAssignRef,
+{
     fn default() -> Self {
         Self {
             mnemonic: String::default(),
             bytes: 0,
             cycles: 0,
+            func: nop,
         }
     }
 }
 
-// pub fn ld_r8_r8(operands: &mut Operands<'_>) {
-//     if let Operands::TwoOneMut(&mut mut x, &y) = *operands {
-//         x = y;
-//     } else {
-//         panic!("Wrong operands passed to function")
-//     }
-// }
+impl<T: NumAssignRef> Instruction<T> {
+    fn exec(&mut self, params: Operands<T>) {
+        (self.func)(params)
+    }
+}
+
+pub fn nop<T: NumAssignRef>(operands: Operands<'_, T>) -> () {}
+
+pub fn ld_rx_rx<T: NumAssignRef>(operands: Operands<'_, T>) {
+    if let Operands::TwoOneMut(x, y) = operands {
+        *x = y
+    }
+}
 
 #[cfg(test)]
 mod instruction_tests {
@@ -37,18 +49,32 @@ mod instruction_tests {
 
     #[test]
     fn test_ld_r8_r8() {
+        let source = 10;
         let mut target = 0;
 
-        let nop = Instruction {
+        let mut instruction = Instruction {
             mnemonic: Default::default(),
             bytes: 0,
             cycles: 4,
+            func: super::ld_rx_rx,
         };
-        let source = 10;
 
-        let mut op = Operands::TwoOneMut(&mut target, &source);
+        instruction.exec(Operands::TwoOneMut(&mut target, source));
 
-        ld_r8_r8(&mut op);
-        assert_eq!(target, 10)
+        assert_eq!(target, source)
+    }
+    fn test_ld_r16_r16() {
+        let source = 30000;
+        let mut target = 0;
+
+        let mut instruction = Instruction {
+            mnemonic: Default::default(),
+            bytes: 0,
+            cycles: 4,
+            func: super::ld_rx_rx,
+        };
+
+        instruction.exec(Operands::TwoOneMut(&mut target, source));
+        assert_eq!(target, source)
     }
 }
