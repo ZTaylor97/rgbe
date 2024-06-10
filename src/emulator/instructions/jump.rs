0ea4924e70da38db36a3fcb9335cd89135b6dd63
ZTaylor97 <zacktaylor97@gmail.com>
2024-06-10 13:55:52 +1000

    GB-17: get_jump_operands WIP

--- src/emulator/instructions/jump.rs
+++ src/emulator/instructions/jump.rs
@@ -1,4 +1,7 @@
-use num_traits::{ops::overflowing::OverflowingSub, WrappingAdd, WrappingSub};
+use num_traits::{
+    ops::overflowing::{OverflowingAdd, OverflowingSub},
+    WrappingAdd, WrappingSub,
+};
 
 use super::utils::{BranchArgs, InstructionError, Operands, Ret, Word};
 use crate::emulator::{
@@ -25,7 +28,38 @@ pub fn jp(operands: Operands<'_>, branch_args: BranchArgs) -> Result<u8, Instruc
             }
             (word1, word2) => {
                 return Err(InstructionError::IncorrectOperandsError(format!(
-                    "Incorrect words {:?} , {:?} passed to add function add",
+                    "Incorrect words {:?} , {:?} passed to jump function",
+                    word1, word2
+                )));
+            }
+        }
+    } else {
+        return Err(InstructionError::InvalidOperandsError(operands));
+    }
+}
+
+pub fn jr(operands: Operands<'_>, branch_args: BranchArgs) -> Result<u8, InstructionError> {
+    if let Operands::Two(target, source, flags) = operands {
+        match (target, source) {
+            (Word::U16Mut(target), Word::U8(source)) => {
+                if let Some(condition) = branch_args.condition {
+                    if jp_check_condition(*flags.unwrap(), condition) {
+                        let val = target.wrapping_add_signed((source as i8) as i16);
+                        *target = val;
+
+                        Ok(branch_args.cycles[0])
+                    } else {
+                        Ok(branch_args.cycles[1])
+                    }
+                } else {
+                    let val = target.wrapping_add_signed((source as i8) as i16);
+                    *target = val;
+                    Ok(branch_args.cycles[0])
+                }
+            }
+            (word1, word2) => {
+                return Err(InstructionError::IncorrectOperandsError(format!(
+                    "Incorrect words {:?} , {:?} passed to jump function",
                     word1, word2
                 )));
             }
@@ -37,4 +71,37 @@ pub fn jp(operands: Operands<'_>, branch_args: BranchArgs) -> Result<u8, Instruc
 
 pub fn jp_check_condition(flags: u8, condition: u8) -> bool {
     (flags & condition) == condition
+}
+
+pub fn get_jump_operands<'a>(
+    registers: &'a mut CPURegisters,
+    mem: &'a mut Memory,
+    opcode: u8,
+    value: Option<Ret>,
+) -> Result<(Operands<'a>, Option<u8>), InstructionError<'a>> {
+    let hi = (opcode & 0xF0) >> 4;
+    let lo = opcode & 0x0F;
+
+    let reg_copy = registers.clone();
+
+    let hl = registers.get_hl();
+    let mem_hl_val = mem.read_u8(hl).clone();
+
+    let dest = Word::U16Mut(&mut registers.pc);
+
+    match opcode {
+        0xC2 => {
+            let address = if let Some(Ret::U16(address)) = value {
+                address
+            } else {
+                return Err(InstructionError::InvalidLiteral(value.unwrap()));
+            };
+            Ok((
+                Operands::Two(dest, Word::U16(address), None),
+                Some(0b1100_0000),
+            ))
+        }
+
+        _ => return Err(InstructionError::UnimplementedError(opcode)),
+    }
 }
